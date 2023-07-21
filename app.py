2a10d7fceb1c15e24fb9e82d0fcdba17a40558ed
Archisman Sengupta <archismansengupta02@gmail.com>
2023-07-21 11:05:21 +0530

    Made some changes to Projects page

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request
 # Creating an instance of Flask class
 app = Flask(__name__, template_folder='templates')   
 
+## Basic Pages:
+
 # Creating a Home Page:
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -19,10 +21,21 @@ def contact():
 def projects():
     return render_template('projects.html')
 
+## Pages related to Projects:
+
 # Creating the Students.com page:
 @app.route('/students')
 def students():
     return render_template('students.html')
+
+# Creating the Kitting Project page:
+@app.route('/kitting')
+def kitting():
+    return render_template('kitting.html')
+
+## Pages related to Blog Page:
+
+
     
 # Running the Flask App:
 if __name__ == '__main__':
